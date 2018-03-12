19c1bdcecd43c9130da2d75d11e852272751bb15
Wende <bzcwende@firepro2.zib.de>
2018-03-12 14:20:10 +0100

    internal restructuring

---
+++ include/misc/misc.hpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2017-2018 Florian Wende (flwende@gmail.com)
+//
+// Distributed under the BSD 2-clause Software License
+// (See accompanying file LICENSE)
+
+#if !defined(MISC_HPP)
+#define MISC_HPP
+
+#include "misc_math.hpp"
+#include "misc_variadic.hpp"
+
+#endif
