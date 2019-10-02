07b4ad64d590eb79c647ae6c6fd50483986a49d4
fwende <fwende@icorner.zib.de>
2019-10-02 14:59:53 +0200

    moved Compare data structure from Traits.hpp into this file

--- include/data_types/DataTypes.hpp
+++ include/data_types/DataTypes.hpp
@@ -17,6 +17,47 @@ namespace XXX_NAMESPACE
 {
     using SizeType = std::size_t;
     using RealType = float;
+
+    namespace dataTypes
+    {
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //!
+        //! \brief Get the data type that is the stronger one (larger in size or 'double' over 'float')
+        //!
+        //! Note: the weaker type is the other one :)
+        //!
+        //! \tparam T_1 some type
+        //! \tparam T_2 some other type
+        //!
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        template <typename T_1, typename T_2, typename Enabled = void>
+        struct Compare
+        {
+            using StrongerType = std::conditional_t<(sizeof(T_1) > sizeof(T_2)), T_1, T_2>;
+            using StrongerTypeUnqualified = std::decay_t<StrongerType>;
+            using WeakerType = std::conditional_t<(sizeof(T_1) > sizeof(T_2)), T_2, T_1>;
+            using WeakerTypeUnqualified = std::decay_t<WeakerType>;
+        };
+
+        template <typename T_1, typename T_2>
+        struct Compare<T_1, T_2, std::enable_if_t<std::is_floating_point<T_1>::value && !std::is_floating_point<T_2>::value>>
+        {
+            using StrongerType = T_1;
+            using StrongerTypeUnqualified = std::decay_t<StrongerType>;
+            using WeakerType = T_2;
+            using WeakerTypeUnqualified = std::decay_t<WeakerType>;
+        };
+
+        template <typename T_1, typename T_2>
+        struct Compare<T_1, T_2, std::enable_if_t<!std::is_floating_point<T_1>::value && std::is_floating_point<T_2>::value>>
+        {
+            using StrongerType = T_2;
+            using StrongerTypeUnqualified = std::decay_t<StrongerType>;
+            using WeakerType = T_1;
+            using WeakerTypeUnqualified = std::decay_t<WeakerType>;
+        };
+    }
 }
 
 #include <data_types/array/Array.hpp>
