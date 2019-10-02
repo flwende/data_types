f52a6fa74a1a5cd18cf6f373881438ca718425a7
fwende <fwende@icorner.zib.de>
2019-10-02 17:28:03 +0200

    check for C++14 or newer

---
+++ include/auxiliary/CPPStandard.hpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2017-2019 Florian Wende (flwende@gmail.com)
+//
+// Distributed under the BSD 2-clause Software License
+// (See accompanying file LICENSE)
+
+#if !defined(__cpp_constexpr)
+static_assert(false, "ERROR: C++14 or newer is needed. Sorry.");
+#endif
