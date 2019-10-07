4dc4d47500d16d65bffa91eda74887c8adfb3e61
fwende <fwende@icorner.zib.de>
2019-10-07 09:43:25 +0200

    generalized Compare implementation

--- include/data_types/DataTypes.hpp
+++ include/data_types/DataTypes.hpp
@@ -19,6 +19,36 @@ namespace XXX_NAMESPACE
 
     namespace dataTypes
     {
+        namespace
+        {
+            template <typename T_1, typename T_2, typename Enabled = void>
+            struct CompareImplementation
+            {
+                using StrongerT = std::conditional_t<(sizeof(T_1) > sizeof(T_2)), T_1, T_2>;
+                using UnqualifiedStrongerT = std::decay_t<StrongerT>;
+                using WeakerT = std::conditional_t<(sizeof(T_1) > sizeof(T_2)), T_2, T_1>;
+                using UnqualifiedWeakerT = std::decay_t<WeakerT>;
+            };
+
+            template <typename T_1, typename T_2>
+            struct CompareImplementation<T_1, T_2, std::enable_if_t<std::is_floating_point<T_1>::value && !std::is_floating_point<T_2>::value>>
+            {
+                using StrongerT = T_1;
+                using UnqualifiedStrongerT = std::decay_t<StrongerT>;
+                using WeakerT = T_2;
+                using UnqualifiedWeakerT = std::decay_t<WeakerT>;
+            };
+
+            template <typename T_1, typename T_2>
+            struct CompareImplementation<T_1, T_2, std::enable_if_t<!std::is_floating_point<T_1>::value && std::is_floating_point<T_2>::value>>
+            {
+                using StrongerT = T_2;
+                using UnqualifiedStrongerT = std::decay_t<StrongerT>;
+                using WeakerT = T_1;
+                using UnqualifiedWeakerT = std::decay_t<WeakerT>;
+            };
+        }
+
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         //!
         //! \brief Get the data type that is the stronger one (larger in size or 'double' over 'float')
@@ -29,31 +59,24 @@ namespace XXX_NAMESPACE
         //! \tparam T_2 some other type
         //!
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        template <typename ...T>
+        struct Compare;
 
-        template <typename T_1, typename T_2, typename Enabled = void>
-        struct Compare
+        template <typename Head, typename ...Tail>
+        struct Compare<Head, Tail...>
         {
-            using StrongerT = std::conditional_t<(sizeof(T_1) > sizeof(T_2)), T_1, T_2>;
+            using StrongerT = typename Compare<Head, typename Compare<Tail...>::StrongerT>::StrongerT;
             using UnqualifiedStrongerT = std::decay_t<StrongerT>;
-            using WeakerT = std::conditional_t<(sizeof(T_1) > sizeof(T_2)), T_2, T_1>;
+            using WeakerT = typename Compare<Head, typename Compare<Tail...>::WeakerT>::WeakerT;
             using UnqualifiedWeakerT = std::decay_t<WeakerT>;
         };
 
         template <typename T_1, typename T_2>
-        struct Compare<T_1, T_2, std::enable_if_t<std::is_floating_point<T_1>::value && !std::is_floating_point<T_2>::value>>
+        struct Compare<T_1, T_2>
         {
-            using StrongerT = T_1;
+            using StrongerT = typename CompareImplementation<T_1, T_2>::StrongerT;
             using UnqualifiedStrongerT = std::decay_t<StrongerT>;
-            using WeakerT = T_2;
-            using UnqualifiedWeakerT = std::decay_t<WeakerT>;
-        };
-
-        template <typename T_1, typename T_2>
-        struct Compare<T_1, T_2, std::enable_if_t<!std::is_floating_point<T_1>::value && std::is_floating_point<T_2>::value>>
-        {
-            using StrongerT = T_2;
-            using UnqualifiedStrongerT = std::decay_t<StrongerT>;
-            using WeakerT = T_1;
+            using WeakerT = typename CompareImplementation<T_1, T_2>::WeakerT;
             using UnqualifiedWeakerT = std::decay_t<WeakerT>;
         };
     }
