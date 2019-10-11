7b34f01e0cfc8d8ab10f80438e0c336466cfce32
fwende <fwende@icorner.zib.de>
2019-10-11 17:20:51 +0200

    WRONG performance bug: std::uint8_t is not supported for SIMD on x86 + enable -DNDEBUG

--- src/include/kernel.hpp
+++ src/include/kernel.hpp
@@ -42,7 +42,8 @@ using element_type = ::fw::tuple<type_x, type_y, type_z>;
 */
 
 using type_x = std::uint16_t;
-using type_y = std::int8_t;
+//using type_y = std::int8_t;
+using type_y = double;
 using type_z = std::uint32_t;
 using element_type = ::fw::dataTypes::Tuple<type_x, type_y, type_z>;
 
