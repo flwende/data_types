328e87a63f8e6eb02a0cade150f273b81c676994
fwende <fwende@icorner.zib.de>
2019-10-24 13:41:36 +0200

    added generic lambda test

--- include/auxiliary/CPPStandard.hpp
+++ include/auxiliary/CPPStandard.hpp
@@ -4,5 +4,9 @@
 // (See accompanying file LICENSE)
 
 #if !defined(__cpp_constexpr)
-static_assert(false, "ERROR: C++14 or newer is needed. Sorry.");
+static_assert(false, "ERROR: (constexpr) C++11 or newer is needed. Sorry.");
+#endif
+
+#if !defined(__cpp_generic_lambdas)
+static_assert(false, "ERROR: (generic lambdas) C++14 or newer is needed. Sorry.");
 #endif
