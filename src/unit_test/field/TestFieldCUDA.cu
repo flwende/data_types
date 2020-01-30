1bf3ca53df12256226c676c855bb83d0f38670f2
Florian Wende <flwende@gmail.com>
2020-01-30 17:44:05 +0100

    formatting; added SoAi and AoSoA test

--- src/unit_test/field/TestFieldCUDA.cu
+++ src/unit_test/field/TestFieldCUDA.cu
@@ -52,129 +52,241 @@ template <typename Container>
 __global__
 void Kernel_2(Container field)
 {
-  const SizeT x = blockIdx.x * blockDim.x + threadIdx.x;
-  const SizeT y = blockIdx.y * blockDim.y + threadIdx.y;
-  const SizeT z = blockIdx.z * blockDim.z + threadIdx.z;
-  const SizeT index = (z * field.Size(1) + y) * field.Size(0) + x;
+    const SizeT x = blockIdx.x * blockDim.x + threadIdx.x;
+    const SizeT y = blockIdx.y * blockDim.y + threadIdx.y;
+    const SizeT z = blockIdx.z * blockDim.z + threadIdx.z;
+    const SizeT index = (z * field.Size(1) + y) * field.Size(0) + x;
 
-  if (x < field.Size(0) && y < field.Size(1) && z < field.Size(2))
+    if (x < field.Size(0) && y < field.Size(1) && z < field.Size(2))
     {
-      field[z][y][x].x = 3 * index + 0;
-      field[z][y][x].y = 3 * index + 1;
-      field[z][y][x].z = 3 * index + 2;      
+        field[z][y][x].x = 3 * index + 0;
+        field[z][y][x].y = 3 * index + 1;
+        field[z][y][x].z = 3 * index + 2;      
     }
 }
 
 TEST(Field, aos_3d_assign_1dindex_as_value)
 {
-  constexpr SizeT nx = 23;
-  constexpr SizeT ny = 7;	
-  constexpr SizeT nz = 13;		
-  
-  Field<ElementT, 3, DataLayout::AoS> field({nx, ny, nz}, true);
-  
-  EXPECT_EQ(true, [&field]() {
-		    bool all_zero = true;
-		    loop_3d(field, [&all_zero](auto&& item) { if (item != 0) all_zero = false; });
-		    return all_zero;
-		  } ());
-  
-  dim3 block{128, 1, 1};
-  dim3 grid{(nx + (block.x - 1)) / block.x, (ny + (block.y - 1)) / block.y, (nz + (block.z - 1)) / block.z};
-    
-  Kernel_1<<<grid, block>>>(field.DeviceData());
-  field.CopyDeviceToHost();
+    constexpr SizeT nx = 23;
+    constexpr SizeT ny = 7;	
+    constexpr SizeT nz = 13;		
 
-  EXPECT_EQ(true, [&field]() {
-		    bool output_is_correct = true;
-		    SizeT index = 0;
-		    loop_3d(field, [&output_is_correct, &index](auto&& item) { if (item != index++) output_is_correct = false; });
-		    return output_is_correct;
-		  } ());
+    Field<ElementT, 3, DataLayout::AoS> field({nx, ny, nz}, true);
+
+    EXPECT_EQ(true, [&field]() {
+        bool all_zero = true;
+        loop_3d(field, [&all_zero](auto&& item) { if (item != 0) all_zero = false; });
+        return all_zero;
+    } ());
+
+    dim3 block{128, 1, 1};
+    dim3 grid{(nx + (block.x - 1)) / block.x, (ny + (block.y - 1)) / block.y, (nz + (block.z - 1)) / block.z};
+
+    Kernel_1<<<grid, block>>>(field.DeviceData());
+    field.CopyDeviceToHost();
+
+    EXPECT_EQ(true, [&field]() {
+        bool output_is_correct = true;
+        SizeT index = 0;
+        loop_3d(field, [&output_is_correct, &index](auto&& item) { if (item != index++) output_is_correct = false; });
+        return output_is_correct;
+    } ());
 }
 
 TEST(Field, aos_3d_assign_1dindex_as_value_componentwise)
 {
-  constexpr SizeT nx = 23;
-  constexpr SizeT ny = 7;	
-  constexpr SizeT nz = 13;		
-  
-  Field<ElementT, 3, DataLayout::AoS> field({nx, ny, nz}, true);
-  
-  EXPECT_EQ(true, [&field]() {
-		    bool all_zero = true;
-		    loop_3d(field, [&all_zero](auto&& item) { if (item != 0) all_zero = false; });
-		    return all_zero;
-		  } ());
-  
-  dim3 block{128, 1, 1};
-  dim3 grid{(nx + (block.x - 1)) / block.x, (ny + (block.y - 1)) / block.y, (nz + (block.z - 1)) / block.z};
+    constexpr SizeT nx = 23;
+    constexpr SizeT ny = 7;	
+    constexpr SizeT nz = 13;		
     
-  Kernel_2<<<grid, block>>>(field.DeviceData());
-  field.CopyDeviceToHost();
+    Field<ElementT, 3, DataLayout::AoS> field({nx, ny, nz}, true);
+    
+    EXPECT_EQ(true, [&field]() {
+                bool all_zero = true;
+                loop_3d(field, [&all_zero](auto&& item) { if (item != 0) all_zero = false; });
+                return all_zero;
+            } ());
+    
+    dim3 block{128, 1, 1};
+    dim3 grid{(nx + (block.x - 1)) / block.x, (ny + (block.y - 1)) / block.y, (nz + (block.z - 1)) / block.z};
+        
+    Kernel_2<<<grid, block>>>(field.DeviceData());
+    field.CopyDeviceToHost();
 
-  EXPECT_EQ(true, [&field]() {
-		    bool output_is_correct = true;
-		    SizeT index = 0;
-		    loop_3d(field, [&output_is_correct, &index](auto&& item) { if (item != ElementT{3 * index + 0, 3 * index + 1, 3 * index + 2}) output_is_correct = false; ++index; });
-		    return output_is_correct;
-		  } ());
+    EXPECT_EQ(true, [&field]() {
+        bool output_is_correct = true;
+        SizeT index = 0;
+        loop_3d(field, [&output_is_correct, &index](auto&& item) { if (item.x != index || item.y != index || item.z != index) output_is_correct = false; ++index; });
+        return output_is_correct;
+    } ());
 }
 
 TEST(Field, soa_3d_assign_1dindex_as_value)
 {
-  constexpr SizeT nx = 23;
-  constexpr SizeT ny = 7;	
-  constexpr SizeT nz = 13;		
-  
-  Field<ElementT, 3, DataLayout::SoA> field({nx, ny, nz}, true);
-  
-  EXPECT_EQ(true, [&field]() {
-		    bool all_zero = true;
-		    loop_3d(field, [&all_zero](auto&& item) { if (item != ElementT(0)) all_zero = false; });
-		    return all_zero;
-		  } ());
-  
-  dim3 block{128, 1, 1};
-  dim3 grid{(nx + (block.x - 1)) / block.x, (ny + (block.y - 1)) / block.y, (nz + (block.z - 1)) / block.z};
-    
-  Kernel_1<<<grid, block>>>(field.DeviceData());
-  field.CopyDeviceToHost();
-  
-  EXPECT_EQ(true, [&field]() {
-		    bool output_is_correct = true;
-		    SizeT index = 0;
-		    loop_3d(field, [&output_is_correct, &index](auto&& item) { if (item.x != index || item.y != index || item.z != index) output_is_correct = false; ++index; });
-		    return output_is_correct;
-		  } ());
+    constexpr SizeT nx = 23;
+    constexpr SizeT ny = 7;	
+    constexpr SizeT nz = 13;		
+
+    Field<ElementT, 3, DataLayout::SoA> field({nx, ny, nz}, true);
+
+    EXPECT_EQ(true, [&field]() {
+        bool all_zero = true;
+        loop_3d(field, [&all_zero](auto&& item) { if (item != ElementT(0)) all_zero = false; });
+        return all_zero;
+    } ());
+
+    dim3 block{128, 1, 1};
+    dim3 grid{(nx + (block.x - 1)) / block.x, (ny + (block.y - 1)) / block.y, (nz + (block.z - 1)) / block.z};
+
+    Kernel_1<<<grid, block>>>(field.DeviceData());
+    field.CopyDeviceToHost();
+
+    EXPECT_EQ(true, [&field]() {
+        bool output_is_correct = true;
+        SizeT index = 0;
+        loop_3d(field, [&output_is_correct, &index](auto&& item) { if (item.x != index || item.y != index || item.z != index) output_is_correct = false; ++index; });
+        return output_is_correct;
+    } ());
 }
 
 TEST(Field, soa_3d_assign_1dindex_as_value_componentwise)
 {
-  constexpr SizeT nx = 23;
-  constexpr SizeT ny = 7;	
-  constexpr SizeT nz = 13;		
-  
-  Field<ElementT, 3, DataLayout::SoA> field({nx, ny, nz}, true);
-  
-  EXPECT_EQ(true, [&field]() {
-		    bool all_zero = true;
-		    loop_3d(field, [&all_zero](auto&& item) { if (item != ElementT(0)) all_zero = false; });		    
-		    return all_zero;
-		  } ());
-  
-  dim3 block{128, 1, 1};
-  dim3 grid{(nx + (block.x - 1)) / block.x, (ny + (block.y - 1)) / block.y, (nz + (block.z - 1)) / block.z};
-    
-  Kernel_2<<<grid, block>>>(field.DeviceData());
-  field.CopyDeviceToHost();
+    constexpr SizeT nx = 23;
+    constexpr SizeT ny = 7;	
+    constexpr SizeT nz = 13;		
 
-  EXPECT_EQ(true, [&field]() {
-		    bool output_is_correct = true;
-		    SizeT index = 0;
-		    loop_3d(field, [&output_is_correct, &index](auto&& item) { if (item.x != (3 * index) || item.y != (3 * index + 1) || item.z != (3 * index + 2)) output_is_correct = false; ++index; });
-		    return output_is_correct;
-		  } ());
+    Field<ElementT, 3, DataLayout::SoA> field({nx, ny, nz}, true);
+
+    EXPECT_EQ(true, [&field]() {
+        bool all_zero = true;
+        loop_3d(field, [&all_zero](auto&& item) { if (item != ElementT(0)) all_zero = false; });		    
+        return all_zero;
+    } ());
+
+    dim3 block{128, 1, 1};
+    dim3 grid{(nx + (block.x - 1)) / block.x, (ny + (block.y - 1)) / block.y, (nz + (block.z - 1)) / block.z};
+
+    Kernel_2<<<grid, block>>>(field.DeviceData());
+    field.CopyDeviceToHost();
+
+    EXPECT_EQ(true, [&field]() {
+        bool output_is_correct = true;
+        SizeT index = 0;
+        loop_3d(field, [&output_is_correct, &index](auto&& item) { if (item.x != (3 * index) || item.y != (3 * index + 1) || item.z != (3 * index + 2)) output_is_correct = false; ++index; });
+        return output_is_correct;
+    } ());
+}
+
+TEST(Field, soai_3d_assign_1dindex_as_value)
+{
+    constexpr SizeT nx = 23;
+    constexpr SizeT ny = 7;	
+    constexpr SizeT nz = 13;		
+
+    Field<ElementT, 3, DataLayout::SoAi> field({nx, ny, nz}, true);
+
+    EXPECT_EQ(true, [&field]() {
+        bool all_zero = true;
+        loop_3d(field, [&all_zero](auto&& item) { if (item != ElementT(0)) all_zero = false; });
+        return all_zero;
+    } ());
+
+    dim3 block{128, 1, 1};
+    dim3 grid{(nx + (block.x - 1)) / block.x, (ny + (block.y - 1)) / block.y, (nz + (block.z - 1)) / block.z};
+
+    Kernel_1<<<grid, block>>>(field.DeviceData());
+    field.CopyDeviceToHost();
+
+    EXPECT_EQ(true, [&field]() {
+        bool output_is_correct = true;
+        SizeT index = 0;
+        loop_3d(field, [&output_is_correct, &index](auto&& item) { if (item.x != index || item.y != index || item.z != index) output_is_correct = false; ++index; });
+        return output_is_correct;
+    } ());
+}
+
+TEST(Field, soai_3d_assign_1dindex_as_value_componentwise)
+{
+    constexpr SizeT nx = 23;
+    constexpr SizeT ny = 7;	
+    constexpr SizeT nz = 13;		
+
+    Field<ElementT, 3, DataLayout::SoAi> field({nx, ny, nz}, true);
+
+    EXPECT_EQ(true, [&field]() {
+        bool all_zero = true;
+        loop_3d(field, [&all_zero](auto&& item) { if (item != ElementT(0)) all_zero = false; });		    
+        return all_zero;
+    } ());
+
+    dim3 block{128, 1, 1};
+    dim3 grid{(nx + (block.x - 1)) / block.x, (ny + (block.y - 1)) / block.y, (nz + (block.z - 1)) / block.z};
+
+    Kernel_2<<<grid, block>>>(field.DeviceData());
+    field.CopyDeviceToHost();
+
+    EXPECT_EQ(true, [&field]() {
+        bool output_is_correct = true;
+        SizeT index = 0;
+        loop_3d(field, [&output_is_correct, &index](auto&& item) { if (item.x != (3 * index) || item.y != (3 * index + 1) || item.z != (3 * index + 2)) output_is_correct = false; ++index; });
+        return output_is_correct;
+    } ());
+}
+
+TEST(Field, aosoa_3d_assign_1dindex_as_value)
+{
+    constexpr SizeT nx = 23;
+    constexpr SizeT ny = 7;	
+    constexpr SizeT nz = 13;		
+
+    Field<ElementT, 3, DataLayout::AoSoA> field({nx, ny, nz}, true);
+
+    EXPECT_EQ(true, [&field]() {
+        bool all_zero = true;
+        loop_3d(field, [&all_zero](auto&& item) { if (item != ElementT(0)) all_zero = false; });
+        return all_zero;
+    } ());
+
+    dim3 block{128, 1, 1};
+    dim3 grid{(nx + (block.x - 1)) / block.x, (ny + (block.y - 1)) / block.y, (nz + (block.z - 1)) / block.z};
+
+    Kernel_1<<<grid, block>>>(field.DeviceData());
+    field.CopyDeviceToHost();
+
+    EXPECT_EQ(true, [&field]() {
+        bool output_is_correct = true;
+        SizeT index = 0;
+        loop_3d(field, [&output_is_correct, &index](auto&& item) { if (item.x != index || item.y != index || item.z != index) output_is_correct = false; ++index; });
+        return output_is_correct;
+    } ());
+}
+
+TEST(Field, aosoa_3d_assign_1dindex_as_value_componentwise)
+{
+    constexpr SizeT nx = 23;
+    constexpr SizeT ny = 7;	
+    constexpr SizeT nz = 13;		
+
+    Field<ElementT, 3, DataLayout::AoSoA> field({nx, ny, nz}, true);
+
+    EXPECT_EQ(true, [&field]() {
+        bool all_zero = true;
+        loop_3d(field, [&all_zero](auto&& item) { if (item != ElementT(0)) all_zero = false; });		    
+        return all_zero;
+    } ());
+
+    dim3 block{128, 1, 1};
+    dim3 grid{(nx + (block.x - 1)) / block.x, (ny + (block.y - 1)) / block.y, (nz + (block.z - 1)) / block.z};
+
+    Kernel_2<<<grid, block>>>(field.DeviceData());
+    field.CopyDeviceToHost();
+
+    EXPECT_EQ(true, [&field]() {
+        bool output_is_correct = true;
+        SizeT index = 0;
+        loop_3d(field, [&output_is_correct, &index](auto&& item) { if (item.x != (3 * index) || item.y != (3 * index + 1) || item.z != (3 * index + 2)) output_is_correct = false; ++index; });
+        return output_is_correct;
+    } ());
 }
 
 int main(int argc, char** argv)
