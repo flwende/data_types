662215b749da5006a30dec42b6883656ce8b6a74
Wende <bzcwende@firepro2.zib.de>
2020-05-04 10:21:38 +0200

    Added Intel OneAPI path

--- build_clang.sh
+++ build_clang.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-export INTELROOT=${HOME}/opt/intel/compilers_and_libraries_2019/linux
+#export INTELROOT=${HOME}/opt/intel/compilers_and_libraries_2019/linux
+export INTELROOT=/dassw/intel/compilers_and_libraries_2019/linux
+#export INTELROOT=${HOME}/opt/intel_oneAPI/inteloneapi/compiler/latest/linux/compiler
 
-make -f ${1} clean && make -f ${1} all
+make -f $@ clean && make -f $@
--- run_clang.sh
+++ run_clang.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-export INTELROOT=${HOME}/opt/intel/compilers_and_libraries_2019/linux
+#export INTELROOT=${HOME}/opt/intel/compilers_and_libraries_2019/linux
+export INTELROOT=/dassw/intel/compilers_and_libraries_2019/linux
+#export INTELROOT=${HOME}/opt/intel_oneAPI/inteloneapi/compiler/latest/linux/compiler
 export LD_LIBRARY_PATH=${INTELROOT}/lib/intel64:${LD_LIBRARY_PATH}
 
 $@
