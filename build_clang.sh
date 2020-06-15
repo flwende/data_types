6c49e3b4dc0aa05091de5d929380b1299c1320f8
Wende <bzcwende@nvram2.zib.de>
2020-06-15 11:35:47 +0200

    Changed setup

--- build_clang.sh
+++ build_clang.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #export INTELROOT=${HOME}/opt/intel/compilers_and_libraries_2019/linux
-export INTELROOT=/dassw/intel/compilers_and_libraries_2019/linux
-#export INTELROOT=${HOME}/opt/intel_oneAPI/inteloneapi/compiler/latest/linux/compiler
+#export INTELROOT=/dassw/intel/compilers_and_libraries_2019/linux
+export INTELROOT=${HOME}/opt/intel_oneAPI/inteloneapi/compiler/latest/linux/compiler
 
 make -f $@ clean && make -f $@
